77ab1cc3dc9250b43944ef787d7305cef22cc40a
Meuk Winkelman <meukwinkelman@outlook.com>
2023-06-19 15:27:15 +0200

    Player can move with joystick

--- src/js/game.js
+++ src/js/game.js
@@ -7,25 +7,33 @@ import {BossScene} from "./ObjectClasses/Scenes/BossScene.js";
 import {Player} from "./ObjectClasses/Characters/Player.js";
 import {EndScene} from "./ObjectClasses/Scenes/EndScene.js";
 import {Boss} from "./ObjectClasses/Characters/Enemy/Boss.js";
+import { Arcade } from "arcade-game"
 
 
 export class Game extends Engine {
-    player
-    scene
+    player;
+    scene;
+
+    #arcade;
+    #joystickListener;
     constructor() {
         super({
             width: visualViewport.width,
             height: visualViewport.height,
         })
         this.start(ResourceLoader).then(() => this.startGame())
-        
-
     }
 
     startGame() {
         localStorage.clear();
         this.scene = "game";
         let testScene = "";
+
+        this.#arcade = new Arcade(this, false, true);
+
+        this.#joystickListener = (e) => this.#joyStickFound(e)
+        document.addEventListener("joystickcreated",  this.#joystickListener)
+
         this.player = new Player('player', 10, new Vector(150, 150), 100, 130, 1, 1, Resources.PlayerFront, CollisionType.Active);
 
         this.addScene('startScene', new StartScene())
@@ -33,8 +41,6 @@ export class Game extends Engine {
         this.addScene('BossScene', new BossScene(this.player, 'gameScene'));
         this.addScene('endScene', new EndScene())
 
-
-        
         if (testScene === "Boss") {
             localStorage.setItem("wood", "true");
             localStorage.setItem("nail", "true");
@@ -50,6 +56,28 @@ export class Game extends Engine {
         }
 
     }
+
+    #joyStickFound(e) {
+        let joystick = this.#arcade.Joysticks[e.detail]
+        console.log(this.#arcade.Joysticks[e.detail])
+
+
+        // debug, this shows you the names of the buttons when they are pressed
+        for (const buttonEvent of joystick.ButtonEvents) {
+            document.addEventListener(buttonEvent, () => console.log(buttonEvent))
+        }
+    }
+
+    onPreUpdate(_engine, _delta) {
+        for (let joystick of this.#arcade.Joysticks) {
+            joystick.update()
+        }
+    }
+
+    disconnect() {
+        document.removeEventListener("joystickcreated", this.#joystickListener)
+    }
+
 }
 
 new Game()
